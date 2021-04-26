077416fe46d8c5709bbd5927e3ae2d4776806abb
Ryan Maas <rdmjobs@live.com>
2021-04-26 15:19:39 -0500

    Finished Question's #3 & #4.

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -6,7 +6,7 @@ console.log("\n");
 console.log("--- Question #1. ---");
 const collection = [];
 console.log(`My music collection is (expect empty array): `, collection);
-console.log("\n");
+console.log("\n"); // End Question #1.
 
 
 // - #2.) Add a function named `addToCollection`. This function should: (DONE)
@@ -20,36 +20,65 @@ const addToCollection = (title, artist, yearPublished) => { // To practice conve
     title: title,
     artist: artist,
     yearPublished: yearPublished,
-  } // End "addition"
+  } // End "addition".
   collection.push(addition);
   return addition;
-}; // End "addToCollection"
-console.log(`Adding album to 'collection' array via 'addToCollection' function: `, addToCollection("Greatest Hits", "The Offspring", 2005));
-console.log(`Showing the 'collection' array (expect 1 object literal in the array): `, collection);
-console.log("\n");
+}; // End "addToCollection".
+console.log(`Adding 'Greatest Hits by The Offspring' to 'collection' array via 'addToCollection' function: `, addToCollection("Greatest Hits", "The Offspring", 2005));
+console.log(`Showing the 'collection' array (expect 1 object literal): `, collection);
+console.log("\n"); // End Question #2.
 
 
 // - #3.) Test the `addToCollection` function:
 //   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
 //   - Console.log each album as added using the returned value.
 //   - After all are added, console.log the `collection` array.
-console.log("--- Question #3. ---");
+console.log("--- Question #3. ---"); // I'll just be listing CD's I bought as a teenager. :)
+console.log(`Adding Album 1 of 6 via 'addToCollection' function: `, addToCollection("Americana", "The Offspring", 1998));
+console.log(`Adding Album 2 of 6 via 'addToCollection' function: `, addToCollection("Smash", "The Offspring", 1994));
+console.log(`Adding Album 3 of 6 via 'addToCollection' function: `, addToCollection("The Sickness", "Disturbed", 2000));
+console.log(`Adding Album 4 of 6 via 'addToCollection' function: `, addToCollection("The Eminem Show", "Eminem", 2002));
+console.log(`Adding Album 5 of 6 via 'addToCollection' function: `, addToCollection("Encore", "Eminem", 2004));
+console.log(`Adding Album 6 of 6 via 'addToCollection' function: `, addToCollection("D12 World", "D12", 2004));
+console.log(`Showing the 'collection' array (expect 7 object literals): `, collection);
+console.log("\n"); // End Question #3.
 
-console.log("\n");
 
-// - #4.) Add a function named `showCollection`. This function should:
-//   - Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
-//   - Console.log the number of items in the array.
+// - #4.) Add a function named `showCollection`. This function should: (DONE)
+//   - Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.) (DONE)
+//   - Console.log the number of items in the array. (DONE)
 //   - Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
+console.log("--- Question #4. ---");
+const showCollection = array => {
+  console.log(`The number of items in this array is: `, array.length);
+  for (let album in collection) {
+    console.log(`${collection.title} by ${collection.artist}, published in ${collection.year}.`)
+  } // End "for album in collection" loop.
+}; // End "showCollection".
+console.log("The 'showCollection' function has been created.  Tested in next question.");
+console.log("\n"); // End Question #4.
+
 
 // - #5.) Test the `showCollection` function.
+console.log("--- Question #5. ---");
+
+console.log("\n"); // End Question #5.
+
 
 // - #6.) Add a function named `findByArtist`. This function should:
 //   - Take in `artist` (a string) parameter
 //   - Create an array to hold any results, empty to start
 //   - Loop through the `collection` and add any objects with a matching artist to the array.
 //   - Return the array with the matching results. If no results are found, return an empty array.
+console.log("--- Question #6. ---");
+
+console.log("\n"); // End Question #6.
+
 
 // - #7.) Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are found.
+console.log("--- Question #7. ---");
+
+console.log("\n"); // End Question #7.
+
 
 // > When testing your functions, write all tests in the JavaScript file!
