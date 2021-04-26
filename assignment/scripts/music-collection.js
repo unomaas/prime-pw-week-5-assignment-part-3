28769285f136b2fc934eff018f11b550fceebe9d
Ryan Maas <rdmjobs@live.com>
2021-04-26 14:20:16 -0500

    Finished Question #2.

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -2,20 +2,40 @@ console.log('***** Music Collection *****')
 console.log("\n");
 
 
-// - #1.) Create a variable `collection` that starts as an empty array.
+// - #1.) Create a variable `collection` that starts as an empty array. (DONE)
 console.log("--- Question #1. ---");
+const collection = [];
+console.log(`My music collection is (expect empty array): `, collection);
 console.log("\n");
 
-// - #2.) Add a function named `addToCollection`. This function should:
-//   - Take in the album's `title`, `artist`, `yearPublished` as input parameters
-//   - Create a new object having the above properties
-//   - Add the new object to the end of the `collection` array
-//   - Return the newly created object
+
+// - #2.) Add a function named `addToCollection`. This function should: (DONE)
+//   - Take in the album's `title`, `artist`, `yearPublished` as input parameters (DONE)
+//   - Create a new object having the above properties (DONE)
+//   - Add the new object to the end of the `collection` array (DONE)
+//   - Return the newly created object (DONE)
+console.log("--- Question #2. ---");
+const addToCollection = (title, artist, yearPublished) => { // To practice conventionally declaring functions, since we can't use concise arrow notation in jQuery, it'd be: function addToCollection(title, artist, yearPublished) {}
+  let addition = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  } // End "addition"
+  collection.push(addition);
+  return addition;
+}; // End "addToCollection"
+console.log(`Adding album to 'collection' array via 'addToCollection' function: `, addToCollection("Greatest Hits", "The Offspring", 2005));
+console.log(`Showing the 'collection' array (expect 1 object literal in the array): `, collection);
+console.log("\n");
+
 
 // - #3.) Test the `addToCollection` function:
 //   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
 //   - Console.log each album as added using the returned value.
 //   - After all are added, console.log the `collection` array.
+console.log("--- Question #3. ---");
+
+console.log("\n");
 
 // - #4.) Add a function named `showCollection`. This function should:
 //   - Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
