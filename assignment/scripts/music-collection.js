42fc53e49d15aa88fea078972ca5bcf642fb1307
Ryan Maas <rdmjobs@live.com>
2021-05-02 15:10:06 -0500

    Updated all code as far as I could.

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -36,12 +36,12 @@ console.log("\n"); // End Question #2.
 //   - After all are added, console.log the `collection` array. (DONE)
 //   - STRETCH 2: Add an array of `tracks` to your album objects. Each track should have a `name` and `duration`. (DONE)
 console.log("--- Question #3. ---"); // I'll just be listing CD's I bought as a teenager. :)
-console.log(`Adding Album 1 of 6 via 'addToCollection' function: `, addToCollection("Americana", "The Offspring", 1998, [{trackName: "Pretty Fly", trackDuration: "3:08"}, {trackName: "Why Don't You Get a Job", trackDuration: "2:52"}, {trackName: "The Kids Aren't Alright", trackDuration: "3:00"}]));
-console.log(`Adding Album 2 of 6 via 'addToCollection' function: `, addToCollection("Smash", "The Offspring", 1994, [{trackName: "Gotta Get Away", trackDuration: "3:52"}, {trackName: "Come Out and Play", trackDuration: "3:17"}, {trackName: "Self Esteem", trackDuration: "4:17"}]));
-console.log(`Adding Album 3 of 6 via 'addToCollection' function: `, addToCollection("The Sickness", "Disturbed", 2000, [{trackName: "The Game", trackDuration: "3:47"}, {trackName: "Stupify", trackDuration: "4:33"}, {trackName: "Down With the Sickness", trackDuration: "4:38"}]));
-console.log(`Adding Album 4 of 6 via 'addToCollection' function: `, addToCollection("The Eminem Show", "Eminem", 2002, [{trackName: "Square Dance", trackDuration: "5:23"}, {trackName: "Without Me", trackDuration: "4:50"}, {trackName: "Superman", trackDuration: "5:50"}]));
-console.log(`Adding Album 5 of 6 via 'addToCollection' function: `, addToCollection("Encore", "Eminem", 2004, [{trackName: "Just Lose It", trackDuration: "4:08"}, {trackName: "Ass Like That", trackDuration: "4:25"}, {trackName: "Mockingbird", trackDuration: "4:10"}]));
-console.log(`Adding Album 6 of 6 via 'addToCollection' function: `, addToCollection("D12 World", "D12", 2004, [{trackName: "My Band", trackDuration: "4:58"}, {trackName: "How Come", trackDuration: "4:09"}, {trackName: "Bizarre", trackDuration: "1:21"}]));
+console.log(`Adding Album 1 of 6 via 'addToCollection' function: `, addToCollection("Americana", "The Offspring", 1998, [{trackNumber: 1, trackName: "Pretty Fly", trackDuration: "3:08"}, {trackNumber: 2, trackName: "Why Don't You Get a Job", trackDuration: "2:52"}, {trackNumber: 3, trackName: "The Kids Aren't Alright", trackDuration: "3:00"}]));
+console.log(`Adding Album 2 of 6 via 'addToCollection' function: `, addToCollection("Smash", "The Offspring", 1994, [{trackNumber: 1, trackName: "Gotta Get Away", trackDuration: "3:52"}, {trackNumber: 2, trackName: "Come Out and Play", trackDuration: "3:17"}, {trackNumber: 3, trackName: "Self Esteem", trackDuration: "4:17"}]));
+console.log(`Adding Album 3 of 6 via 'addToCollection' function: `, addToCollection("The Sickness", "Disturbed", 2000, [{trackNumber: 1, trackName: "The Game", trackDuration: "3:47"}, {trackNumber: 2, trackName: "Stupify", trackDuration: "4:33"}, {trackNumber: 3, trackName: "Down With the Sickness", trackDuration: "4:38"}]));
+console.log(`Adding Album 4 of 6 via 'addToCollection' function: `, addToCollection("The Eminem Show", "Eminem", 2002, [{trackNumber: 1, trackName: "Square Dance", trackDuration: "5:23"}, {trackNumber: 2, trackName: "Without Me", trackDuration: "4:50"}, {trackNumber: 3, trackName: "Superman", trackDuration: "5:50"}]));
+console.log(`Adding Album 5 of 6 via 'addToCollection' function: `, addToCollection("Encore", "Eminem", 2004, [{trackNumber: 1, trackName: "Just Lose It", trackDuration: "4:08"}, {trackNumber: 2, trackName: "Ass Like That", trackDuration: "4:25"}, {trackNumber: 2, trackName: "Mockingbird", trackDuration: "4:10"}]));
+console.log(`Adding Album 6 of 6 via 'addToCollection' function: `, addToCollection("D12 World", "D12", 2004, [{trackNumber: 1, trackName: "My Band", trackDuration: "4:58"}, {trackNumber: 2, trackName: "How Come", trackDuration: "4:09"}, {trackNumber: 3, trackName: "Bizarre", trackDuration: "1:21"}]));
 console.log(`Showing the 'collection' array (expect '6' results): `, collection);
 console.log("\n"); // End Question #3.
 
@@ -58,11 +58,11 @@ console.log("\n"); // End Question #3.
 console.log("--- Question #4. ---");
 const showCollection = array => {
   console.log(`The number of albums in this list is: `, array.length);
-  let albumCount = 1; // This wasn't requested, but I'd like my console.log to include an Album Count (e.g., "Album #1 is ..." etc.), so I'm adding a counter into the function. :)
+  let albumCount = 1;
   for (let i in array) {
-    console.log(`Album #${albumCount}: \n'${collection[i].title}' by ${collection[i].artist}, published in ${collection[i].yearPublished}: \n${collection[i].tracks}`);
+      console.log(`Album #${albumCount}: \n'${collection[i].title}' by ${collection[i].artist}, published in ${collection[i].yearPublished}: \n${collection[i].tracks[0].trackNumber}. ${collection[i].tracks[0].trackName}: ${collection[i].tracks[0].trackDuration} \n${collection[i].tracks[1].trackNumber}. ${collection[i].tracks[1].trackName}: ${collection[i].tracks[1].trackDuration} \n${collection[i].tracks[2].trackNumber}. ${collection[i].tracks[2].trackName}: ${collection[i].tracks[2].trackDuration}`); // Need a nested loop to work here.
     albumCount++;
-  } // End "for i in array" loop.
+  } // End for i loop.
 }; // End "showCollection".
 console.log("The 'showCollection' function has been created.  Tested in next question.");
 console.log("\n"); // End Question #4.
@@ -110,32 +110,41 @@ console.log("\n"); // End Question #7.
 //   - If there is no search object or an empty search object provided as input, then return all albums in the `collection`. (DONE)
 //   - STRETCH #2: Update `search` to allow a `trackName` search criteria.
 console.log("--- Stretch #1. ---");
-const search = object => { // Have to figure out how to pass an object as an argument.
+const search = object => {
   let results = [];
-  if (object == "" || object == null || object == undefined) { // If empty search critera conditional:
+  if (object == "" || object == null || object == undefined || object.length == 0) { // If empty search critera conditional:
     return collection; // Return all albums in 'collection'.
-  } // End empty search criteria conditional.
-  // if (typeof object == "string") { // If the search critera is a string: // Technically this was never part of the question.
-    for (let i in collection) {
-      if (object == collection[i].artist || object == collection[i].title || object == collection[i].yearPublished || object == object == collection[i].tracks) { // If string matches any of the artist, title, yearPublished, OR tracks:
+  } else if (typeof object == "string") { // Else if the search critera is a string:
+    for (let i in collection) { // Loop through the collection array's object's indices:
+      if (object == collection[i].artist || object == collection[i].title || object == collection[i].yearPublished || object == collection[i].tracks[i]) { // If string matches any of the artist, title, yearPublished, OR tracks:
         results.push(collection[i]); // Add it to the new 'results' array.
       } // End "if object == collection" string conditional.
     } // End "for i in collection" loop.
-  // } // End "if typeof == string" conditional.
-  // if (typeof object == "object") { // If the search critera is an object:
-  //   // .filter?  .find? .includes?  .indexOf?  Need to figure out how to use passed object literals in comparisons.
-  // } // End "if typeof == object" conditional.
+  } else if (typeof object === "object") { // Else if the search critera is an object:
+    for (let criteria of collection) {
+      if (criteria.artist === object.artist) { // If artist comparison:
+        results.push(criteria);
+      } else if (criteria.artist === object.artist && critera.yearPublished === object.yearPublished) { // Else if artist and yearPublished comparison:
+        results.push(criteria);
+      } else if (criteria.title === object.title && critera.yearPublished === object.yearPublished) { // Else if title and yearPublished comparison:
+        results.push(criteria);
+      } else if (criteria.title === object.title) { // Else if title comparison:
+        results.push(criteria);
+      }
+    } // End "for critera of collection" loop.
+  } // End "if typeof == object" conditional.
   return results;
 }; // End "search".
 console.log("The 'search' function has been created.  Tested below.");
-console.log(`Testing 'search("Eminem")' function (expect '2' results): `, search("Eminem"));
-console.log(`Testing 'search("2004")' function (expect '2' results): `, search("2004"));
-console.log(`Testing 'search("The Sickness")' function (expect '1' result): `, search("The Sickness"));
 console.log(`Testing 'search()' function empty conditional (expect '6' results): `, search());
 console.log(`Testing 'search("")' function empty conditional (expect '6' results): `, search(""));
-console.log(`Testing 'search({artist: "Eminem"})' function (expect '1' results): `, search({artist: "Eminem"})); // Broken, I need to figure out how to search with key value pairs... I think?
-// console.log(`Testing 'search("Square Dance")' function (expect '1' result): `, search("Square Dance")); // This will never work because I have the track names hardcoded with time and numbers.
-// console.log(`Testing 'search("Eminem", 2002)' function (expect '1' results): `, search("Eminem", 2002)); // Broken, is pulling two results.  // This shouldn't work anyways.
+console.log(`Testing 'search("Eminem")' string conditional (expect '2' results): `, search("Eminem"));
+console.log(`Testing 'search("2004")' string conditional (expect '2' results): `, search("2004"));
+console.log(`Testing 'search("The Sickness")' string conditional (expect '1' result): `, search("The Sickness"));
+console.log(`Testing 'search({artist: "Eminem"})' object conditional (expect '2' results): `, search({artist: "Eminem"}));
+console.log(`Testing 'search({artist: "Eminem", yearPublished: 2002})' object conditional (expect '1' results): `, search({artist: "Eminem", yearPublished: 2002})); // This is broken, should only return one result, is returning two.
+// console.log(`Testing 'search({title: "The Eminem Show", yearPublished: 2002})' object conditional (expect '1' results): `, search({title: "The Eminem Show", yearPublished: 2002})); // This is broken, should kick one result.
+// console.log(`Testing 'search({title: "The Eminem Show"})' object conditional (expect '1' results): `, search({title: "The Eminem Show"})); // This is broken, giving me a "criteria is not defined" error.
 console.log("\n"); // End Stretch #1.
 
 
@@ -153,79 +162,3 @@ console.log("\n"); // End Stretch #1.
 console.log("--- Stretch #2. ---");
 console.log("All of the functionality requests have been built into the code, above.")
 console.log("\n"); // End Stretch #2.
-
-console.log("Tests below.");
-let artistTest = collection.filter(artist => collection.artist == "Eminem");
-console.log(artistTest);
-
-
-
-
-
-
-// Save code below
-
-// console.log("--- Stretch #1. ---");
-// const search = (artist, year) => {
-//   if ((artist == "" || year == "") || (artist == null && year == null) || (artist == undefined && year == undefined)) { // If empty search critera conditional:
-//     return collection; // Return all albums in 'collection'.
-//   } // End empty search criteria conditional.
-//   let results = [];
-//   for (let i in collection) {
-//     if ((artist == collection[i].artist || artist == collection[i].title || artist == collection[i].yearPublished) && (year == collection[i].artist || year == collection[i].title || year == collection[i].yearPublished)) { // If (artist matches any of the artist, title, OR yearPublished info) AND (year matches any of the artist, title, OR yearPublished info):
-//       results.push(collection[i]); // Add it to the new 'results' array.
-//     } // End "if object == collection" conditional.
-//   } // End "for i in collection" loop.
-//   return results;
-// }; // End "search".
-// console.log("The 'search' function has been created.  Tested below.");
-// console.log(`Testing 'search("Eminem")' function (expect '2' results): `, search("Eminem")); // Broken by adding year parameter.
-// console.log(`Testing 'search("2004")' function (expect '2' results): `, search("2004")); // Broken by adding year parameter.
-// console.log(`Testing 'search("The Sickness")' function (expect '1' result): `, search("The Sickness")); // Broken by adding year parameter.
-// console.log(`Testing 'search()' function empty conditional (expect '6' results): `, search());
-// console.log(`Testing 'search("")' function empty conditional (expect '6' results): `, search(""));
-// console.log(`Testing 'search("Eminem", 2002)' function (expect '1' results): `, search("Eminem", 2002));
-// console.log("\n"); // End Stretch #1.
-
-
-// ### Stretch goals
-// - #S1.) Create a function called `search`. This function should:
-//   - Take an input parameter for a search criteria object. The search criteria might look sosomething like this:
-//      { artist: 'Ray Charles', year: 1957 }
-//   - Return a new array of all items in the `collection` matching *all* of the search criteria. (DONE)
-//   - If no results are found, return an empty array. (DONE)
-//   - If there is no search object or an empty search object provided as input, then return all albums in the `collection`. (DONE)
-//   - STRETCH #2: Update `search` to allow a `trackName` search criteria.
-// console.log("--- Stretch #1. ---");
-// const search = (artist, year) => { // There's got to be a way to do a partial keyword search...
-//   if ((artist == "" || year == "") || (artist == null && year == null) || (artist == undefined && year == undefined)) { // If empty search critera conditional:
-//     return collection; // Return all albums in 'collection'.
-//   } // End empty search criteria conditional.
-//   let results = [];
-//   for (let i in collection) {
-//     if ((artist == collection[i].artist || artist == collection[i].title || artist == collection[i].yearPublished) || (year == collection[i].artist || year == collection[i].title || year == collection[i].yearPublished)) { // If (artist matches any of the artist, title, OR yearPublished info) AND (year matches any of the artist, title, OR yearPublished info):
-//       results.push(collection[i]); // Add it to the new 'results' array.
-//     } // End "if object == collection" conditional.
-//   } // End "for i in collection" loop.
-//   return results;
-// }; // End "search".
-// console.log("The 'search' function has been created.  Tested below.");
-// console.log(`Testing 'search("Eminem")' function (expect '2' results): `, search("Eminem"));
-// console.log(`Testing 'search("2004")' function (expect '2' results): `, search("2004"));
-// console.log(`Testing 'search("The Sickness")' function (expect '1' result): `, search("The Sickness"));
-// console.log(`Testing 'search()' function empty conditional (expect '6' results): `, search());
-// console.log(`Testing 'search("")' function empty conditional (expect '6' results): `, search(""));
-// console.log(`Testing 'search("Eminem", 2002)' function (expect '1' results): `, search("Eminem", 2002)); // Broken, is pulling two results.
-// console.log("\n"); // End Stretch #1.
-
-
-// console.log("--- Tests, Delete After ---");
-// // Ryan's Comments: So I have an array of objects.  The array is "collection", and each object is an album with TITLE, ARTIST, and YEAR inside.  I need to access the array --> object --> property specifically.  What's the syntax for that.
-// // console.log(array[i]); // This works and logs each object as it's listed in the array, but not in the format they want.
-// console.log(collection);
-// console.log(collection[0]);
-// console.log(collection[0].title);
-// console.log(collection[0].artist);
-// console.log(collection[0].yearPublished);
-// console.log("\n"); // End Tests.
-// > When testing your functions, write all tests in the JavaScript file!
